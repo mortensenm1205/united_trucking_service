cf088ad164ab48061d40cda41603870f344a3cf3
Matt Mortensen <mortensenm1205@gmail.com>
2018-09-30 17:51:44 -0600

    Root only suports /about route

--- src/Screens/Root.js
+++ src/Screens/Root.js
@@ -1,9 +1,13 @@
-import React, { Componet } from 'react';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import About from '../Components/About';
 
 class ScreenRoot extends Component {
   render() {
     return(
-      
+      <Switch>
+        <Route path="/about" component={About} />
+      </Switch>
     )
   }
 }
