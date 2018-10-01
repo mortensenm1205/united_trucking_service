d6e99b3cd9e922da191739e02d247a45e78868d2
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-10-01 14:28:55 -0600

    bringing in Contact

---
+++ src/Components/Contact/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ContactForm = props => {
+  return(
+    <form>
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <button>Contact</button>
+    </form>
+  )
+}
+
+export default ContactForm;
---
+++ src/Components/Contact/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ContactForm from './Form';
+
+class ContactContainer extends Component {
+  render() {
+    return(
+      <ContactForm />
+    )
+  }
+}
+
+export default ContactContainer;
