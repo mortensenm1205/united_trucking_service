176cec713eea854294f36160d1ef497b47a20e4c
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-10-01 14:29:02 -0600

    displaying Contact

--- src/Screens/Root.js
+++ src/Screens/Root.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import About from '../Components/About';
+import Contact from '../Components/Contact';
 
 class ScreenRoot extends Component {
   render() {
     return(
       <Switch>
         <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
       </Switch>
     )
   }
