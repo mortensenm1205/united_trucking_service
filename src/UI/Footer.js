2c7164c683d822d6dfee54cbad4a5a7c30e66a01
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-10-01 16:13:32 -0600

    updating footer

--- src/UI/Footer.js
+++ src/UI/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = props => {
   return (
-    <p>&copy;2017 by United Truck Service. Proudly created by Mayan Mobile Marketing</p>
+    <p>&copy;2017 by United Truck Service. Proudly created by ImagineTech</p>
   )
 }
 
