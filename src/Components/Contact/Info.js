99f6dcafd5b920804ded37719ae08301f938e259
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-10-01 14:39:45 -0600

    bringing in Info Comp, small

---
+++ src/Components/Contact/Info.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ContactInfo = props => {
+  return(
+    <div>
+      <span>For any questions or concerns, Please Feel Free to contact us:</span>
+      <h3>Towing: (908) 224-0060</h3>
+      <h3>Sales/Parts: (908) 361-0376</h3>
+    </div>
+  )
+}
+
+export default ContactInfo;
