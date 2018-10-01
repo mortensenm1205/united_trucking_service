8363a4181e765a62b7d251d5334be4ed9c18e365
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-10-01 15:34:02 -0600

    displaying header/footer

--- src/Screens/Root.js
+++ src/Screens/Root.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Header from '../UI/Header';
+import Footer from '../UI/Footer';
 import About from '../Components/About';
 import Contact from '../Components/Contact';
 
 class ScreenRoot extends Component {
   render() {
     return(
-      <Switch>
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+        <Footer />
+      </div>
     )
   }
 }
