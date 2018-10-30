8318279c1d75696238701bc68550d3146ec7e29f
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-10-30 15:41:46 -0600

    Adding content for services

--- src/Components/Services/index.js
+++ src/Components/Services/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import ServicesContent from './Content';
 
 class ServicesContainer extends Component {
     render() {
         return (
             <div id="services">
                 <h1>I am the Services Portion</h1>
+                <ServicesContent />
             </div>
         )
     }
