bba508626d6f1a19452c9132a3a88496263d537e
Matt Mortensen <mortensenm1205@gmail.com>
2018-11-03 18:38:36 -0600

    Home now seperate comp from root

---
+++ src/Components/Home/Content.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomeContent = props => {
+    return (
+        <p>United Truck Service in Central New Jersey, offers heavy duty towing in Hillsborough, Middlesex County, Somerset County, Bridgewater, Far Hills, Manville, Peapack-Gladstone, Raritan and the nearby area. Offering 24 hour towing, United Truck Service is here for you.</p>
+    )
+}
+
+export default HomeContent;
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import Header from '../Header';
-import Footer from '../Footer';
+import React, { Component } from 'react';
+import HomeContent from './Content';
 
-const HomeContainer = ({ children }) => {
-    return (
-        <div>
-            <Header />
-             {children}
-            <Footer />
-        </div>
-    )
+class HomeContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h1>“HOME PAGE” TITLE</h1>
+                <HomeContent />
+            </div>
+        )
+    }
 }
 
 export default HomeContainer;
