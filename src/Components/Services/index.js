f41ff25b20417c768c8e6d038b7212c666804564
Matt Mortensen <mortensenm1205@gmail.com>
2018-11-04 09:19:05 -0700

    updating for services

--- src/Components/Services/index.js
+++ src/Components/Services/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import ServicesContent from './Content';
+import ServiceTypes from './Types/';
 
 class ServicesContainer extends Component {
     render() {
         return (
             <div>
                 <h1>I am the Services Portion</h1>
-                <ServicesContent />
+                <ServiceTypes {...this.props} />
             </div>
         )
     }
