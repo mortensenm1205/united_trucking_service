532fb8cea191b6f7b333045c7f07ad9dcf4eaafb
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-11-05 12:52:53 -0700

    wreckerService now routed. all services now routed

--- src/Screens/Services.js
+++ src/Screens/Services.js
@@ -4,6 +4,7 @@ import Types from '../Components/Services/Types';
 import HeavyDutyTowing from '../Components/Services/Types/HeavyDutyTowing';
 import CarTowing from '../Components/Services/Types/CarTowing';
 import RoadsideAssistance from '../Components/Services/Types/Roadside';
+import WreckerService from '../Components/Services/Types/WreckerService';
 
 class ScreenServices extends Component {
     render() {
@@ -13,6 +14,7 @@ class ScreenServices extends Component {
                 <Route path="/services/heavyDutyTowing" component={HeavyDutyTowing} />
                 <Route path="/services/carTowing" component={CarTowing} />
                 <Route path="/services/roadsideAssistance" component={RoadsideAssistance} />
+                <Route path="/services/wreckerService" component={WreckerService} />
             </Switch>
         )
     }
