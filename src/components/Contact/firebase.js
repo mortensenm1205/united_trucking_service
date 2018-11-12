114bc2a0d121af4486b8a588d68a4dbafeecd201
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-11-12 16:04:03 -0700

    having firebase now be it's own directory for outside use

--- src/components/Contact/firebase.js
+++
@@ -1,9 +0,0 @@
-import { db } from '../../config/firebase';
-
-export const addContactData = contact_obj => {
-    return db.collection('contact_info').add({
-        Name: contact_obj.name,
-        Email: contact_obj.email,
-        Message: contact_obj.message
-    })
-}
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,40 +1,41 @@
-import React, { Component } from 'react';
-import ContactForm from './Form';
-import ContactInfo from './Info';
-import ContactSuccess from './Success';
-import RootContainer from '../Root';
-import { addContactData } from './firebase';
+import React, { Component } from "react";
+import ContactForm from "./Form";
+import ContactInfo from "./Info";
+import ContactSuccess from "./Success";
+import RootContainer from "../Root";
+import { addContactData } from "./firebase/";
 
 class ContactContainer extends Component {
-
   state = {
     contact_values: {},
     success: false
-  }
+  };
 
   submit = e => {
     const { contact_values, success } = this.state;
     e.preventDefault();
     e.target.reset();
-    addContactData(contact_values).then(doc => doc ? this.setState({ success: !success }) : null );
-  }
+    addContactData(contact_values).then(doc =>
+      doc ? this.setState({ success: !success }) : null
+    );
+  };
 
   values = e => {
     const { contact_values } = this.state;
     const { name, value } = e.target;
-    this.setState({ contact_values: {...contact_values, [name]: value} })
-  }
+    this.setState({ contact_values: { ...contact_values, [name]: value } });
+  };
 
   render() {
     const { success } = this.state;
-    return(
+    return (
       <RootContainer>
         <h1>“CONTACT” PAGE TITLE</h1>
         <ContactForm submit={this.submit} values={this.values} />
         <ContactSuccess success={success} />
         <ContactInfo />
       </RootContainer>
-    )
+    );
   }
 }
 
