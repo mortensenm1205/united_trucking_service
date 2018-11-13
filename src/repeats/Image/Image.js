398cd4eac739db51970de66f97ff2716abf2ec2a
Matt Mortensen <mortensenm1205@gmail.com>
2018-11-13 12:12:48 -0700

    adding: image files for simple comp and fb

---
+++ src/repeats/Image/Image.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Image = () => {
+    return (
+        <figure>
+            <img src="" alt="im_the_huge_header_img" />
+        </figure>
+    )
+}
+
+export default Image;
---
+++ src/repeats/Image/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Image from './Image';
+
+class ImageContainer extends Component {
+    render() {
+        return(
+            <div>
+                <Image />
+            </div>
+        )
+    }
+}
+
+export default ImageContainer;
