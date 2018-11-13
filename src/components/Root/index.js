5cd71b0a7883330c62269e58cf83cebb8790d12c
Matt Mortensen <mortensenm1205@gmail.com>
2018-11-13 15:47:30 -0700

    testing location pathname values

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -1,17 +1,27 @@
-import React from "react";
-import HeaderImage from "../../repeats/Image/";
+import React, { Component } from "react";
+import { downloadImage } from '../../repeats/Image';
 import Header from "../../repeats/Header";
 import Footer from "../../repeats/Footer";
+import { withRouter } from 'react-router-dom';
 
-const RootContainer = ({ children }) => {
-  return (
-    <div>
-      <Header />
-      <HeaderImage />
-        {children}
-      <Footer />
-    </div>
-  )
+class RootContainer extends Component {
+
+  componentDidUpdate(nextProps) {
+    if(nextProps.location !== this.props.location) {
+      console.log(this.props.location.pathname);
+    };
+  }
+
+  render() {
+    const { children } = this.props
+    return (
+      <div>
+        <Header />
+          {children}
+        <Footer />
+      </div>
+    )
+  }
 };
 
-export default RootContainer;
+export default withRouter(RootContainer);
