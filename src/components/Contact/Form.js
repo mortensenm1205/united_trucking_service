a7969add88310f0c99a880e8519488ae48768e08
Shubham Tiwari <websbiz0000@gmail.com>
2018-11-17 21:40:09 +0530

    adds some form validation

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -3,9 +3,9 @@ import React from 'react';
 const ContactForm = ({ submit, values }) => {
   return(
     <form onSubmit={submit}>
-      <input type="text" name="name" onChange={values} />
-      <input type="text" name="email" onChange={values} />
-      <input type="text" name="message" onChange={values} />
+      <input type="text" name="name" onChange={values} required/>
+      <input type="email" name="email" onChange={values} required/>
+      <input type="text" name="message" onChange={values} required/>
       <button>Contact</button>
     </form>
   )
