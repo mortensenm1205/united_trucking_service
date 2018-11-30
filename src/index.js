91a110db9290ab9de299f593bc2ba18b09293faa
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-11-30 16:17:18 -0700

    styled: added normalize

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createBrowserHistory } from "history";
 import { Router } from "react-router-dom";
 import { render } from "react-dom";
+import 'normalize.css'
 import App from "./screens/Root";
 
 const history = createBrowserHistory();
