c795a405b6c1faf046cd3a76037fba11e07ef728
Matt Mortensen <mortensenm1205@gmail.com>
2018-12-03 20:05:33 -0700

    Removed flexbox, going with raw css layout tech.

--- src/repeats/Header/Info.js
+++ src/repeats/Header/Info.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import { HeadInfoContainer, HeadInfoImageContainer, HeadInfoContent } from './header_css';
+import { HeadInfoContainer, HeadInfoImageContainer } from './header_css';
 
 const HeaderInfo = () => {
     return (
         <HeadInfoContainer>
-            <HeadInfoContent>
-                <h3>SALES, PARTS & SANDBLASTING</h3>
-                <p>(908) 361-0376</p>
-            </HeadInfoContent>
             <HeadInfoImageContainer>
                 <img className="header_logo" src="https://firebasestorage.googleapis.com/v0/b/united-trucking-company.appspot.com/o/dummy_banner.jpg?alt=media&token=cf949563-0832-4b16-add7-d0549ab9612a" alt="United Truck Service Logo" />
             </HeadInfoImageContainer>
-            <HeadInfoContent>
-                <h3>TOWING</h3>
-                <p>(908) 224-0060</p>
-            </HeadInfoContent>
         </HeadInfoContainer>
     );
 }
