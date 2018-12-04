3aa0b9960f06c8cbbd8397b64a0dc8c2faedcd03
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-12-04 12:49:36 -0700

    styled: layout for home content page

--- src/components/Home/Content.js
+++ src/components/Home/Content.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { HomeCont } from './home_css';
 
 const HomeContent = props => {
     return (
-        <section>
+        <HomeCont>
             <p>United Truck Service in Central New Jersey, offers heavy duty towing in Hillsborough, Middlesex County, Somerset County, Bridgewater, Far Hills, Manville, Peapack-Gladstone, Raritan and the nearby area. Offering 24 hour towing, United Truck Service is here for you.</p>
             <p>Towing services include: </p>
             <ul>
@@ -13,7 +14,7 @@ const HomeContent = props => {
                 <li>Wrecker Service</li>
             </ul>
             <p>Getting you back on the road safely and quickly is United Truck Service’s priority. United Truck Service has a team of experienced certified professionals that are ready to recover your vehicle and deliver it safely to your destination. Contact them at (908) 224-0060. Reliability and customer satisfaction are important to United Truck Service. The company takes pride in providing you a valuable towing service in Central New Jersey. And as always, first time customers receive 10% off. If you or someone you know is looking for a professional, reliable, towing company, United Truck Service has you covered. Contact United Truck Service at (908) 224-0060. </p>
-        </section>
+        </HomeCont>
     )
 }
 
--- src/components/Home/home_css.js
+++ src/components/Home/home_css.js
@@ -13,6 +13,13 @@ export const HomeImg = styled.img.attrs(props => ({
     position: absolute;
     top: 0;
     left: 0;
-    width: 100 %;
+    width: 100%;
     height: auto;
 `
+
+export const HomeCont = styled.div`
+    width: 40%;
+    float: left;
+    margin: 0 3% 0 0;
+    line-height: 1.6;
+`;
