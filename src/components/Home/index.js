89e39692b0aa95c73f94ce7882b20b595ef8a318
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-12-04 10:16:37 -0700

    firebase: updated just home cont. to use api call for images

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import { downloadImage } from '../../repeats/Image';
 import HomeContent from './Content';
+import HomeImage from './Image';
 
 class HomeContainer extends Component {
+
+    state = {
+        homeImageURL: ""
+    }
+
+    componentDidMount() {
+        downloadImage(this.props.location.pathname).then(url => {
+            this.setState({ homeImageURL: url })
+        })
+    }
+
     render() {
+        const { location } = this.props;
+        const { homeImageURL } = this.state;
         return (
             <div>
                 <h1>UNITED TRUCKING SERVICE</h1>
                 <HomeContent />
+                <HomeImage url={homeImageURL} location={location} />
             </div>
         )
     }
