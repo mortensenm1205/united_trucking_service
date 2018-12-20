947c22268a0ffdb8aea9f923d043161b957d2651
Matt Mortensen <mortensenm1205@gmail.com>
2018-12-20 09:54:52 -0700

    refactor: media queries for font-size and vh height

--- src/components/Root/root_css.js
+++ src/components/Root/root_css.js
@@ -12,13 +12,24 @@ export const RootContain = styled.div`
     margin: 0 auto;
     
     & > div {
-        min-height: 45vh;
+        min-height: 100vh;
 
         h1 {
-            font-size: 2.6rem;
+            font-size: 1.8rem;
             text-align: center;
             color: #a11e35;
-            margin: 5% 0;
+            margin: 5% 0 7%;
+        }
+
+        @media (min-width: 850px) {
+            div {
+                min-height: 50vh;
+            }
+
+            h1 {
+                font-size: 2.6rem;
+                margin: 5% 0;
+            }
         }
     }
 `;
