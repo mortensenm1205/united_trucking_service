84ff7f2e7fd2a5b572a0ab65d03928df241b5855
Matt Mortensen <mortensenm1205@gmail.com>
2018-12-20 19:09:32 -0700

    updates: image and style updates.

--- src/components/Root/root_css.js
+++ src/components/Root/root_css.js
@@ -11,24 +11,30 @@ export const RootContain = styled.div`
     margin: 0 auto;
     
     & > div {
-        min-height: 130vh;
+        min-height: 145vh;
+    }
 
-        h1 {
-            font-size: 1.8rem;
-            text-align: center;
-            color: #a11e35;
-            margin: 5% 0 7%;
+    h1 {
+        font-size: 1.8rem;
+        text-align: center;
+        color: #a11e35;
+        margin: 5% 0 7%;
+    }
+
+    @media (min-width: 750px) {
+        & > div {
+            min-height: 110vh;
+        }
+    }
+
+    @media (min-width: 1100px) {
+        & > div {
+            min-height: 150vh;
         }
 
-        @media (min-width: 1100px) {
-            div {
-                min-height: 50vh;
-            }
-
-            h1 {
-                font-size: 2.6rem;
-                margin: 5% 0;
-            }
+        h1 {
+            font-size: 2.6rem;
+            margin: 5% 0;
         }
     }
 `;
--- src/repeats/Header/Info.js
+++ src/repeats/Header/Info.js
@@ -4,7 +4,7 @@ import { HeadInfoContainer, HeadInfoImage } from './header_css';
 const HeaderInfo = () => {
     return (
         <HeadInfoContainer>
-            <HeadInfoImage src="https://firebasestorage.googleapis.com/v0/b/united-trucking-company.appspot.com/o/uts_logo.png?alt=media&token=81f83abe-79a4-4668-8b9e-720a8fc7e2c6"  />
+            <HeadInfoImage src="https://firebasestorage.googleapis.com/v0/b/united-trucking-company.appspot.com/o/uts_logo.png?alt=media&token=dcaee300-b331-46df-a867-d0bad99b94ef"  />
         </HeadInfoContainer>
     );
 }
