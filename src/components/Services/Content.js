e3b14783ed21b2981d56d8a46d1588449be633fe
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2019-01-21 11:05:40 -0700

    small content update, pushed to prod

--- src/components/Services/Content.js
+++ src/components/Services/Content.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const ServicesContent = props => {
     return (
-        <p></p>
+        <div>
+            <p>United Truck Service in Central New Jersey, offers heavy duty towing in Hillsborough, Middlesex County, Somerset County, Bridgewater, Far Hills, Manville, Peapack-Gladstone, Raritan and the nearby area. Offering 24 hour towing, United Truck Service is here for you.</p>
+            <p>Towing services include:</p>
+        </div>
     )
 }
 
